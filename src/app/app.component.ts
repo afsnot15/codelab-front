69e76020ff63c7cdb9175553341b2825bb34ca6e
afsnot15 <afsnot15@hotmail.com>
2024-04-23 23:16:16 -0300

    Correcao Logins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'cl-root',
   standalone: true,
-  imports: [HomeComponent, LayoutComponent],
+  imports: [HomeComponent, LayoutComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
