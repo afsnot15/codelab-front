0a36cbf85f9daaef59b476d42415659698495a4e
afsnot15 <afsnot15@hotmail.com>
2024-04-23 23:34:27 -0300

    Ajuste code review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'cl-root',
   standalone: true,
-  imports: [HomeComponent, LayoutComponent, LoginComponent],
+  imports: [HomeComponent, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
